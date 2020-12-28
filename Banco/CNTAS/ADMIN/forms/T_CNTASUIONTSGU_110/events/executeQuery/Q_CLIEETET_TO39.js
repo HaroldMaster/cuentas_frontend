8aa1d8882e9d2be91dece3bd198947729b781ce0
Harold Flores <bhflores1994@gmail.com>
2020-12-28 10:15:30 -0500

    Se agrega primera parte del formulario creacion cuenta

---
+++ Banco/CNTAS/ADMIN/forms/T_CNTASUIONTSGU_110/events/executeQuery/Q_CLIEETET_TO39.js
@@ -0,0 +1,6 @@
+
+//ClienteQuery Entity: Cliente
+task.executeQuery.Q_CLIEETET_TO39 = function(executeQueryEventArgs){
+    executeQueryEventArgs.commons.execServer = true;
+    //executeQueryEventArgs.commons.serverParameters.Cliente = true;
+};
