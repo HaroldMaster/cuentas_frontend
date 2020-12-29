97fe91a27727332ddc57fb9fe1027b5d70cb5987
Harold Flores <bhflores1994@gmail.com>
2020-12-29 11:09:49 -0500

    se añade un cliente a las tablas de cuentas ahorros y corriente

--- Banco/CNTAS/ADMIN/forms/T_CNTASUIONTSGU_110/events/executeCommand/CM_TCNTASUI_SNN.js
+++ Banco/CNTAS/ADMIN/forms/T_CNTASUIONTSGU_110/events/executeCommand/CM_TCNTASUI_SNN.js
@@ -4,7 +4,9 @@
 // (Button) 
 task.executeCommand.CM_TCNTASUI_SNN = function(entities, executeCommandEventArgs) {
     executeCommandEventArgs.commons.execServer = true;
+   // executeCommandEventArgs.parameters.codigocl = executeCommandEventArgs.commons.api.grid.getData("QV_TV39_HMX33")[0].idCliente;
+   entities.Cuenta.cliente = executeCommandEventArgs.commons.api.grid.getData("QV_TV39_HMX33")[0].idCliente;
     
     //executeCommandEventArgs.commons.serverParameters.entityName = true;
-
+    
 };
